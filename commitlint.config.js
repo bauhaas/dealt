ec4a2f9e0dc269b9154636d018d00823fb748c70
Korkrane <baudoin.haas@gmail.com>
2024-05-24 16:55:53 +0200

    chore(): add rules for commit type with commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,57 @@
-export default {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+    "type-case": [2, "always", "lower-case"],
+    "type-empty": [2, "never"],
+    "subject-empty": [2, "never"],
+    "subject-case": [0],
+    "custom-message": [2, "always"],
+  },
+  plugins: [
+    {
+      rules: {
+        "custom-message": ({ type }) => {
+          const types = [
+            "build",
+            "chore",
+            "ci",
+            "docs",
+            "feat",
+            "fix",
+            "perf",
+            "refactor",
+            "revert",
+            "style",
+            "test",
+          ];
+          if (!types.includes(type)) {
+            return [
+              false,
+              `Invalid type: "${type}". Use one of the following types: ${types.join(
+                ", "
+              )}.`,
+            ];
+          }
+          return [true];
+        },
+      },
+    },
+  ],
+};
